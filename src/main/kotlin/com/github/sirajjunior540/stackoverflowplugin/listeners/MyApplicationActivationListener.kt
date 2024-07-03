cfc05265d19c165d68356969580e0a8b07e3bd30
GitHub Action <action@github.com>
2024-07-03 08:18:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/sirajjunior540/stackoverflowplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/sirajjunior540/stackoverflowplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sirajjunior540.stackoverflowplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
